36de0669a755d5ecbac4a498460253da6c0204bb
Fixer33 <44867167+Fixer33@users.noreply.github.com>
2025-08-06 04:13:16 -0700

    Update UIViewFactoryBase.cs

--- Runtime/UIViewFactoryBase.cs
+++ Runtime/UIViewFactoryBase.cs
@@ -11,24 +11,60 @@ namespace UI
         public GameObject[] CreateViewPrefabArray(GameObject[] basePrefabs)
         {
             GameObject[] result = new GameObject[basePrefabs.Length];
-            var viewTypes = _viewPrefabOverrides.Select(i => i.GetComponent<IView>().GetType()).ToArray();
+            var viewOverrides = _viewPrefabOverrides
+                .Select(i => i.GetComponent<IView>())
+                .Where(i => i is not null)
+                .ToArray();
             for (var i = 0; i < basePrefabs.Length; i++)
             {
-                int overrideIndex =
-                    Array.IndexOf(viewTypes, basePrefabs[i].GetComponent<IView>().GetType());
+                var prefabView = basePrefabs[i].GetComponent<IView>();
 
-                if (overrideIndex < 0)
+                int overrideIndexInArray = -1;
+                
+                for (var overrideIndex = 0; overrideIndex < viewOverrides.Length; overrideIndex++)
+                {
+                    if (prefabView.GetType() != viewOverrides[overrideIndex].GetType())
+                        continue;
+                    
+                    if (prefabView is IMultipleViewInstance prefabMultipleViewInstance)
+                    {
+                        if (viewOverrides[overrideIndex] is not IMultipleViewInstance overrideMultipleViewInstance)
+                            continue;
+                        
+                        if (prefabMultipleViewInstance.GetMultipleViewId() != overrideMultipleViewInstance.GetMultipleViewId())
+                            continue;
+
+                        overrideIndexInArray = overrideIndex;
+                        break;
+                    }
+                    else
+                    {
+                        overrideIndexInArray = overrideIndex;
+                    }
+                }
+                
+
+                if (overrideIndexInArray < 0)
                 {
                     result[i] = basePrefabs[i];
                     continue;
                 }
 
-                result[i] = _viewPrefabOverrides[overrideIndex];
+                result[i] = viewOverrides[overrideIndexInArray].GameObject;
             }
 
             return result;
         }
 
         public abstract bool CanBeUsed();
+
+        private void OnValidate()
+        {
+            for (var i = 0; i < _viewPrefabOverrides.Length; i++)
+            {
+                if (_viewPrefabOverrides[i].GetComponent<IView>().IsAlive() == false)
+                    Debug.LogError($"Prefab on index {i} in {name} failed to validate as an IView");
+            }
+        }
     }
 }
