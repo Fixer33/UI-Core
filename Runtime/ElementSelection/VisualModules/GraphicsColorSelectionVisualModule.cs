773d0ac234a4975d5706327203b04cdcf4a37f7b
Ivan <arouba61@gmail.com>
2026-05-13 10:01:47 +0400

    Added overrides to graphics color svm

--- Runtime/ElementSelection/VisualModules/GraphicsColorSelectionVisualModule.cs
+++ Runtime/ElementSelection/VisualModules/GraphicsColorSelectionVisualModule.cs
@@ -8,10 +8,14 @@ namespace UI.ElementSelection.VisualModules
     {
         [SerializeField] private Graphic _graphic;
         [SerializeField] private Color _defaultColor, _selectedColor;
-
+        private Color? _defaultColorOverride, _selectedColorOverride;
+        
         public void OnSelectionChanged(bool isSelected)
         {
-            _graphic.color = isSelected ? _selectedColor : _defaultColor;
+            _graphic.color = isSelected ? (_selectedColorOverride ?? _selectedColor) : (_defaultColorOverride ?? _defaultColor);
         }
+        
+        public void SetDefaultColorOverride(Color color) => _defaultColorOverride = color;
+        public void SetSelectedColorOverride(Color color) => _selectedColorOverride = color;
     }
 }
